e536a22234f580ce0f05da757dbc62a103a2d9a9
Lucas Limeira <lucasalveslm@gmail.com>
2023-01-30 12:49:09 -0300

    refactor: change time entry functions to extend pointer

--- internal/time_entry/time_entry_repository.go
+++ internal/time_entry/time_entry_repository.go
@@ -23,26 +23,26 @@ func NewTimeEntryRepository(provider TimeEntryProvider) TimeEntryRepository {
 	return TimeEntryRepository{provider: provider}
 }
 
-func (t TimeEntryRepository) Create(title string, startDate time.Time) (int, error) {
+func (t *TimeEntryRepository) Create(title string, startDate time.Time) (int, error) {
 	return t.provider.Create(title, startDate)
 }
 
-func (t TimeEntryRepository) Delete(id int) error {
+func (t *TimeEntryRepository) Delete(id int) error {
 	return t.provider.Delete(id)
 }
 
-func (t TimeEntryRepository) GetTags(id int) ([]tag.Tag, error) {
+func (t *TimeEntryRepository) GetTags(id int) ([]tag.Tag, error) {
 	return t.provider.GetTags(id)
 }
 
-func (t TimeEntryRepository) AddTags(id int, tags []tag.Tag) error {
+func (t *TimeEntryRepository) AddTags(id int, tags []tag.Tag) error {
 	return t.provider.AddTags(id, tags)
 }
 
-func (t TimeEntryRepository) RemoveTags(id int, tags []tag.Tag) error {
+func (t *TimeEntryRepository) RemoveTags(id int, tags []tag.Tag) error {
 	return t.provider.RemoveTags(id, tags)
 }
 
-func (t TimeEntryRepository) UpdateTitle(id int, title string) error {
+func (t *TimeEntryRepository) UpdateTitle(id int, title string) error {
 	return t.provider.UpdateTitle(id, title)
 }
